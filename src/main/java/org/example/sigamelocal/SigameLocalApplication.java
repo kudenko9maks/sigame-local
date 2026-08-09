79c9cd9f587b70624159d3cd327686ae62098799
Maksim Kudenko <kudenko9maks@gmail.com>
2026-08-09 13:14:23 +0300

    Initial Spring Boot project

---
+++ src/main/java/org/example/sigamelocal/SigameLocalApplication.java
@@ -0,0 +1,13 @@
+package org.example.sigamelocal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SigameLocalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SigameLocalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/sigamelocal/SigameLocalApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.sigamelocal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SigameLocalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
